c381c7b63c1e80de0dee4e2fd59c7a1b9dfc003a
VanHC <vanhc@galaxy.com.vn>
2022-11-15 14:20:28 +0700

    init sourceee

---
+++ index.js
@@ -0,0 +1,34 @@
+const TeleBot = require("telebot");
+const puppeteer = require("puppeteer");
+const path = require("path");
+const fs = require("fs");
+
+const bot = new TeleBot("5492681556:AAGx6MC2bK6422g9y9tLyDOX9oKkF_5EFwE");
+
+bot.on("/do", (data) => {
+  const { entities } = data;
+  const entityUrl = entities.find((e) => e.type === "url");
+});
+
+bot.on("text", async (data) => {
+  const { entities, text } = data;
+  const entityUrl = entities.find((e) => e.type === "url");
+  if (!entityUrl) return;
+
+  const url = text.substring(entityUrl.offset, entityUrl.length);
+  if (!url) return;
+
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url, {
+    waitUntil: ["load", "domcontentloaded", "networkidle0", "networkidle2"],
+  });
+  const screenshot = path.join("screenshot", "screenshot.png");
+  await page.screenshot({ path: screenshot });
+  await browser.close();
+  const photo = fs.readFileSync(screenshot);
+  data.reply.photo(photo);
+  data.reply.text("Done!")
+});
+
+bot.start();
